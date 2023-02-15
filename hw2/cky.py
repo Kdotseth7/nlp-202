1618ee32360438074b31ce9e6c8ad937dfb53b57
KUSHAGRA SETH <kushagraseth.1996@gmail.com>
2023-02-14 18:48:34 -0800

    CKY implementation

--- hw2/cky.py
+++ hw2/cky.py
@@ -21,34 +21,31 @@ class CKY:
             cky_matrix.append(row)
             
         for j in range(len(words)):
-            # Add non-terminals for each terminal to cell
+            # Add non-terminals for each terminal to cell[j][j]
             cell = grammar.bottom_up_map[words[j]]
             cky_matrix[j][j] = cell
             
-            # go through every previous position
+            # For each cell in the matrix, go through all possible combinations
             for i in reversed(range(j)):
-                sub_entry_set = set()
+                # Sub-Constituent Set is the set of all sub-constituents
+                sub_constituent_set = set()
 
-                # go through every possible combination
-                # in the interval from i to j
                 for k in range(i, j):
-                    # left
+                    # Row Set: cell[i][k]
                     row_set = cky_matrix[i][k]
-                    # right
+                    # Col Set: cell[k + 1][j]
                     col_set = cky_matrix[k + 1][j]
-                    # get the cross of 2 set in order to get all combinations of sub-constituents
-                    cross_set = list(itertools.product(row_set, col_set))
+                    # Cartesian (Cross) Product of row_set and col_set yields all combinations of sub-constituents
+                    cartesian_product = itertools.product(row_set, col_set)
+                    cartesian_product_tuple = tuple(cartesian_product)
 
-                    for left, right in cross_set:
-                        sub_constituent = f"{left} {right}"
-                        # check if the rule exist
-                        if sub_constituent in grammar.bottom_up_map.keys():
-                            sub_entry_set = sub_entry_set.union(
-                                grammar.bottom_up_map[sub_constituent]
-                            )
-                # put all possible entries in to the table
-                cky_matrix[i][j] = sub_entry_set
-            
+                    for (left, right) in cartesian_product_tuple:
+                        sub_constituent = left + " " + right
+                        # If the sub-constituent is in the grammar, add it to the set
+                        if sub_constituent in grammar.bottom_up_map:
+                            sub_constituent_set = sub_constituent_set.union(grammar.bottom_up_map[sub_constituent])
+                # Add the set of sub-constituents to the cell
+                cky_matrix[i][j] = sub_constituent_set
             
         return cky_matrix
     
--- hw2/main.py
+++ hw2/main.py
@@ -4,12 +4,14 @@ from grammar import Grammar
 from tabulate import tabulate
 from cky import CKY
 
+# Parse command-line arguments
 optparser = optparse.OptionParser()
 optparser.add_option("--cky", dest="cky", action="store_true", help="Use CKY algorithm for parsing")
 optparser.add_option("--pcfg-cky", dest="pcfg_cky", action="store_true", help="Use PCFG CKY algorithm for parsing")
 (opts, _) = optparser.parse_args()
 
 def run_cky():
+    # Rules in Chomsky Normal Form(CNF)
     rules = [
                 Rule("S", "NP VP"),
                 Rule("NP", "JJ NP"),
@@ -25,16 +27,15 @@ def run_cky():
                 Rule("P", "on"),
                 Rule("NP", "Falklands")
             ]
-    # for i in rules:
-    #     print(f"{i.left} -> {i.right}")
-        
+    # Grammar: A set of rules
     grammar = Grammar(rules)
-    print(f"Top-Down: {grammar.top_down_map}")
-    print(f"Bottom-Up: {grammar.bottom_up_map}")
     
+    # Sentence to parse
     sentence = "British left waffles on Falklands"
+    # Split the sentence into words
     words = sentence.split(" ")
     
+    # Perform CKY parsing on the sentence
     cky_table = CKY.cky(words, grammar)
     print("================================CKY Table================================")
     print(tabulate(cky_table, headers=words, tablefmt="fancy_grid", showindex="always"))
